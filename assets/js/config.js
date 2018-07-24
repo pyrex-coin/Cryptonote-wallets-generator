193a178983e1244640ffeb6cf609c371c73f313c
RomyToklio <contact@evaveo.com>
2018-07-24 09:59:51 +0200

    update

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,10 +1,108 @@
 var CONFIG_COINS = [
     {
+        name: 'Toklio',
+        symbol: 'TOKL',
+        unitPlaces: 12,
+        uriPrefix: 'Toklio:',
+        addressPrefix: 0x269d
+    },
+    {
+        name: 'Recoal',
+        symbol: 'RECL',
+        unitPlaces: 8,
+        uriPrefix: 'recoal:',
+        addressPrefix: 0xd6
+    },
+    {
         name: 'Pyrexcoin',
         symbol: 'PYX',
         unitPlaces: 8,
         uriPrefix: 'pyrexcoin:',
         addressPrefix: 0xe2386
+    },
+    {
+        name: 'SuperiorCoin',
+        symbol: 'SUP',
+        unitPlaces: 8,
+        uriPrefix: 'SuperiorCoin:',
+        addressPrefix: 26
+    },
+    {
+        name: 'Monero',
+        symbol: 'XMR',
+        unitPlaces: 12,
+        uriPrefix: 'monero:',
+        addressPrefix: 18
+    },
+    {
+        name: 'Ombre',
+        symbol: 'OMB',
+        unitPlaces: 9,
+        uriPrefix: 'ombre:',
+        addressPrefix: 0xe1f54
+    },
+    /*{
+        name: 'Sumokoin',
+        symbol: 'SUMO',
+        unitPlaces: 9,
+        uriPrefix: 'sumokoin:',
+        addressPrefix: 0x2bb39a
+    },*/
+    {
+        name: 'Solace',
+        symbol: 'SOL',
+        unitPlaces: 7,
+        uriPrefix: 'solace:',
+        addressPrefix: 0x211f
+    },
+    {
+        name: 'MonetaVerde',
+        symbol: 'MCN',
+        unitPlaces: 12,
+        uriPrefix: 'solace:',
+        addressPrefix: 0x1A2B
+    },
+    {
+        name: 'Graft',
+        symbol: 'GRFT',
+        unitPlaces: 10,
+        uriPrefix: 'graft:',
+        addressPrefix: 90
+    },
+    {
+        name: 'ItaloCoin',
+        symbol: 'ITA',
+        unitPlaces: 12,
+        uriPrefix: 'italocoin:',
+        addressPrefix: 250
+    },
+    {
+        name: 'Karbo',
+        symbol: 'KRB',
+        unitPlaces: 12,
+        uriPrefix: 'karbo:',
+        addressPrefix: 111
+    },
+    {
+        name: 'Masari',
+        symbol: 'MSR',
+        unitPlaces: 12,
+        uriPrefix: 'karbo:',
+        addressPrefix: 111
+    },
+    {
+        name: 'MoneroV',
+        symbol: 'XMV',
+        unitPlaces: 11,
+        uriPrefix: 'monerov:',
+        addressPrefix: 18
+    },
+    {
+        name: 'Wownero',
+        symbol: 'WOW',
+        unitPlaces: 11,
+        uriPrefix: 'wownero:',
+        addressPrefix: 4146
     }
 ];
 var CONFIG_LANG = ["dutch","electrum","english","esperanto","french","german","italian","japanese","lojban","portuguese","russian","spanish"];
